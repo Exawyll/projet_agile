baad9cb4d080ee6ba6bdd3d366be2f81c9f72dc5
Maurice Arnaud <arnaud.maurice.pro@gmail.com>
2017-06-21 16:20:14 +0200

    vérification saisie utilisateur

--- src/fr/calculator/core/Calculator.java
+++ src/fr/calculator/core/Calculator.java
@@ -4,52 +4,56 @@ import java.util.Scanner;
 
 public class Calculator {
 
-	public static void main(String[] args) {
-		getOperation();
-	}
-	
-	public static void calc(float nb1, float nb2, String operator) {
-		float result;
-		if(operator.equals("+")) {
-			result = nb1+nb2;
-		} else if (operator.equals("-")) {
-			result = nb1-nb2;
-		} else if (operator.equals("*")) {
-			result = nb1*nb2;
-		} else {
-			result = nb1/nb2;
-		}
-
-		String 	operation = nb1 + operator + nb2;
-		
-		printResult(operation, String.valueOf(result));
-		
-	}
-	
-	public static void getOperation() {
-		Scanner reader = new Scanner(System.in);
-		float nb1 = 0;
-		float nb2 = 0;
-		String operator = null;
-		
-		System.out.println("Veuillez saisir votre premier nombre : ");
-		nb1 = reader.nextFloat();
-		
-		if (nb1 == (float)nb1) {
-			System.out.println("Veuillez saisir l'opérateur : ");
-			operator = reader.next();
-			
-			if (operator == (String)operator) {
-				System.out.println("Veuillez saisir votre second nombre : ");
-				nb2 = reader.nextFloat();
-			}
-		}
-		reader.close();
-		calc(nb1, nb2, operator);
-	}
-
-	public static void printResult(String operation, String result) {
-        System.out.printf("Le résultat de l'opération " + operation + " est : " + result);
+    public static void main(String[] args) {
+        getOperation();
     }
+
+    public static void calc(float nb1, float nb2, String operator) {
+        float result;
+        if (operator.equals("+")) {
+            result = nb1 + nb2;
+        } else if (operator.equals("-")) {
+            result = nb1 - nb2;
+        } else if (operator.equals("*")) {
+            result = nb1 * nb2;
+        } else {
+            result = nb1 / nb2;
+        }
+
+        String operation = nb1 + operator + nb2;
+
+        printResult(operation, String.valueOf(result));
+
+    }
+
+    public static void getOperation() {
+        Scanner reader = new Scanner(System.in);
+        float nb1 = 0;
+        float nb2 = 0;
+        String operator = null;
+
+        System.out.println("Veuillez saisir votre premier nombre : ");
+        nb1 = reader.nextFloat();
+
+        if (nb1 == (float) nb1) {
+            while (operator != "+" || operator != "-" || operator != "*" || operator != "/") {
+                System.out.println("Veuillez saisir l'opï¿½rateur : ");
+                operator = reader.next();
+            }
+
+            if (operator == (String) operator) {
+                System.out.println("Veuillez saisir votre second nombre : ");
+                nb2 = reader.nextFloat();
+            }
+        }
+        reader.close();
+        calc(nb1, nb2, operator);
+    }
+
+    public static void printResult(String operation, String result) {
+        System.out.printf("Le rï¿½sultat de l'opï¿½ration " + operation + " est : " + result);
+    }
+
+    private void function
 
 }

d006bbb7e3ee632d5b45f9807505aa613276f37d
romain bizeul <romain.bizeul@gmail.com>
2017-06-21 15:12:44 +0200

    Get user values

--- src/fr/calculator/core/Calculator.java
+++ src/fr/calculator/core/Calculator.java
@@ -1,10 +1,31 @@
 package fr.calculator.core;
 
+import java.util.Scanner;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		getOperation();
 	}
-
+	
+	public static String getOperation () {
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Veuillez saisir votre premier nombre : ");
+		double nb1 = reader.nextDouble();
+		
+		if (nb1 == (double)nb1) {
+			System.out.println("Veuillez saisir l'opérateur : ");
+			String operator = reader.toString();
+			
+			if (operator == (String)operator) {
+				System.out.println("Veuillez saisir votre secobd nombre : ");
+				double nb2 = reader.nextDouble();
+			}
+		}
+		
+		return null;
+	}
 }
